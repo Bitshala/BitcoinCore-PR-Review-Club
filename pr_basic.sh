5556a2ce1200517fe2c57648fddf5f61885b2f7a
rajarshimaitra <rajarshi149@gmail.com>
2022-11-20 18:07:30 +0530

    Add dependency installation scripts

--- pr_basic.sh
+++ pr_basic.sh
@@ -7,19 +7,28 @@ git clone git@github.com:bitcoin/bitcoin.git
 # Check into the cloned repository
 cd bitcoin
 
-# Add the PR author's fork of bitcoin as a remote
-git add andrewtoth git@github.com:andrewtoth/bitcoin.git
+# Add the PR author's fork of bitcoin as a remote.
+# In this case https://github.com/bitcoin/bitcoin/pull/26533
+git remote add andrewtoth git@github.com:andrewtoth/bitcoin.git
 
-# Fetch the PR author
+# Fetch everything from the PR author
 git fetch andrewtoth
 
-# Checkout the particular PR. In this case https://github.com/bitcoin/bitcoin/pull/26533
+# Checkout the particular PR.
 git checkout andrewtoth/scan-and-unlink-pruned-files
 
 
 # See the PR diff in your difttool editor. You can set your custom IDE too.
 git difftool HEAD~1 HEAD
 
+# Install basic dependencies
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3
+
+# Install libboost tools
+sudo apt-get install libevent-dev libboost-dev
+
+# Instal sqlite for wallet
+sudo apt install libsqlite3-dev
 
 # Run autogen.sh
 ./autogen.sh
